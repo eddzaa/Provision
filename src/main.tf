700276d7c94c9d83620cfe796db5411a0f05fe84
edvinaustindsouza <edvinaustindsouza@gmail.com>
2024-03-13 14:09:15 -0400

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -4,16 +4,6 @@ resource "aws_instance" "ec2" {
   key_name      = var.key_name
   ami           = var.ami
   monitoring    = var.monitoring
-
   tags = var.tags
-
-  dynamic "launch_template" {
-    for_each = length(var.launch_template) > 0 ? [var.launch_template] : []
-
-    content {
-      id      = lookup(var.launch_template, "id", null)
-      name    = lookup(var.launch_template, "name", null)
-      version = lookup(var.launch_template, "version", null)
-    }
-  }
+  
 }
